f6b2dfc5be455440b8d5f573b4f9f6f65b52f9d7
ldmallqui <ldmallqui@hotmail.com>
2016-10-04 20:43:01 -0500

    avance de mallqui

--- atencionPostulante/build/generated-sources/ap-source-output/atencion/clases/Postulante_.java
+++ atencionPostulante/build/generated-sources/ap-source-output/atencion/clases/Postulante_.java
@@ -4,27 +4,27 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-03T23:03:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-04T13:43:58")
 @StaticMetamodel(Postulante.class)
 public class Postulante_ { 
 
-    public static volatile SingularAttribute<Postulante, String> centroTrabajo;
-    public static volatile SingularAttribute<Postulante, String> distrito;
-    public static volatile SingularAttribute<Postulante, Long> codPostulante;
-    public static volatile SingularAttribute<Postulante, String> fechaNacimiento;
-    public static volatile SingularAttribute<Postulante, String> fechaRegistro;
+    public static volatile SingularAttribute<Postulante, String> lugarNacimiento;
     public static volatile SingularAttribute<Postulante, String> estadoCivil;
+    public static volatile SingularAttribute<Postulante, String> domicilio;
+    public static volatile SingularAttribute<Postulante, Long> codPostulante;
+    public static volatile SingularAttribute<Postulante, String> distrito;
+    public static volatile SingularAttribute<Postulante, String> sexo;
+    public static volatile SingularAttribute<Postulante, Integer> telefonoCasa;
+    public static volatile SingularAttribute<Postulante, String> fechaRegistro;
+    public static volatile SingularAttribute<Postulante, String> FactorRh;
+    public static volatile SingularAttribute<Postulante, String> fechaNacimiento;
     public static volatile SingularAttribute<Postulante, String> nombre;
+    public static volatile SingularAttribute<Postulante, Integer> DNI;
+    public static volatile SingularAttribute<Postulante, String> centroTrabajo;
     public static volatile SingularAttribute<Postulante, Integer> edad;
     public static volatile SingularAttribute<Postulante, String> tipoDonacion;
-    public static volatile SingularAttribute<Postulante, String> FactorRh;
-    public static volatile SingularAttribute<Postulante, Integer> telefonoCasa;
-    public static volatile SingularAttribute<Postulante, String> domicilio;
-    public static volatile SingularAttribute<Postulante, String> GrupoSanguineo;
     public static volatile SingularAttribute<Postulante, String> Ocupacion;
     public static volatile SingularAttribute<Postulante, Integer> celular;
-    public static volatile SingularAttribute<Postulante, String> sexo;
-    public static volatile SingularAttribute<Postulante, String> lugarNacimiento;
-    public static volatile SingularAttribute<Postulante, Integer> DNI;
+    public static volatile SingularAttribute<Postulante, String> GrupoSanguineo;
 
 }
--- atencionPostulante/build/generated-sources/ap-source-output/atencion/clases/Usuario_.java
+++ atencionPostulante/build/generated-sources/ap-source-output/atencion/clases/Usuario_.java
@@ -4,12 +4,12 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-03T23:03:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-04T13:43:58")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
-    public static volatile SingularAttribute<Usuario, String> password;
-    public static volatile SingularAttribute<Usuario, String> usuario;
     public static volatile SingularAttribute<Usuario, Long> id;
+    public static volatile SingularAttribute<Usuario, String> usuario;
+    public static volatile SingularAttribute<Usuario, String> password;
 
 }
